f70ff6be62b3016f4085fe49dc5d8f755b5e69fe
Adrodoc <adrodoc55@googlemail.com>
2020-11-10 13:59:33 +0100

    Exercise 2-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![3, 6, 7, 5, 2, 1, 4, 8];
+    quicksort(&mut v);
+    println!("{:?}", v);
+}
+
+fn quicksort<T: Clone + PartialOrd>(v: &mut [T]) {
+    let len = v.len();
+    if len > 1 {
+        let pivot = v[0].clone();
+        let mut left = 0;
+        let mut right = len - 1;
+        while left <= right {
+            while v[left] < pivot {
+                left += 1;
+            }
+            while v[right] > pivot {
+                right -= 1;
+            }
+            if left <= right {
+                v.swap(left, right);
+                left += 1;
+                if right != 0 {
+                    right -= 1;
+                }
+            }
+        }
+        quicksort(&mut v[..=right]);
+        quicksort(&mut v[left..]);
+    }
 }
