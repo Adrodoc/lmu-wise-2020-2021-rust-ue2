40f35b500e8e62d751caec6152fa1d38e8d97744
Adrodoc <adrodoc55@googlemail.com>
2020-11-11 11:46:35 +0100

    Add println

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::fmt::Debug;
+
 fn main() {
     let mut v = vec![3, 6, 7, 5, 2, 1, 4, 8];
     quicksort(&mut v);
     println!("{:?}", v);
 }
 
-fn quicksort<T: Clone + PartialOrd>(v: &mut [T]) {
+fn quicksort<T: Clone + PartialOrd + Debug>(v: &mut [T]) {
     let len = v.len();
     if len > 1 {
         let pivot = v[len / 2].clone();
+        println!("run quicksort({:?}) -> pivot={:?}", v, pivot);
         let mut left = 0;
         let mut right = len - 1;
         while left <= right {
@@ -19,6 +22,7 @@ fn quicksort<T: Clone + PartialOrd>(v: &mut [T]) {
             }
             if left <= right {
                 v.swap(left, right);
+                println!("swap({:?}, {:?}) -> {:?}", v[right], v[left], v);
                 left += 1;
                 right -= 1;
             }
