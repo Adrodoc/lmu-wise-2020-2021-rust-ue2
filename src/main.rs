ba1c813600f92bc9f3761631540f217a0c87ae6d
Adrodoc <adrodoc55@googlemail.com>
2020-11-11 12:03:14 +0100

    Add implementation for hoare partition scheme

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::fmt::Debug;
 
 fn main() {
-    let mut v = vec![3, 6, 7, 5, 2, 1, 4, 8];
-    quicksort(&mut v);
-    println!("{:?}", v);
+    let v = vec![3, 6, 7, 5, 2, 1, 4, 8];
+
+    println!("quicksort:");
+    let mut v1 = v.clone();
+    quicksort(&mut v1);
+    println!("{:?}", v1);
+
+    println!("quicksort_hoare:");
+    let mut v2 = v.clone();
+    quicksort_hoare(&mut v2);
+    println!("{:?}", v2);
 }
 
 fn quicksort<T: Clone + PartialOrd + Debug>(v: &mut [T]) {
@@ -29,5 +37,31 @@ fn quicksort<T: Clone + PartialOrd + Debug>(v: &mut [T]) {
         }
         quicksort(&mut v[..=right]);
         quicksort(&mut v[left..]);
+    }
+}
+
+fn quicksort_hoare<T: Clone + PartialOrd + Debug>(v: &mut [T]) {
+    let len = v.len();
+    if len > 1 {
+        let pivot = v[len / 2].clone();
+        println!("run quicksort({:?}) -> pivot={:?}", v, pivot);
+        let mut left = 0;
+        let mut right = len - 1;
+        loop {
+            while v[left] < pivot {
+                left += 1;
+            }
+            while v[right] > pivot {
+                right -= 1;
+            }
+            if left < right {
+                v.swap(left, right);
+                println!("swap({:?}, {:?}) -> {:?}", v[right], v[left], v);
+            } else {
+                break;
+            }
+        }
+        quicksort_hoare(&mut v[..right]);
+        quicksort_hoare(&mut v[right..]);
     }
 }
